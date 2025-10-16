ad14caee6631ab3871e85ee6fa98cf87a8f6e189
fikrialfjri <fikribusiness2021@gmail.com>
2025-10-16 20:50:01 +0700

    Create authentication guard with passport-jwt

---
+++ server/src/common/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from 'src/constants/common';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.SECRET_KEY,
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.sub },
+    });
+
+    if (!user) return null;
+
+    return user;
+  }
+}
