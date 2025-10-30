d993bda023733d0b42867c61cd4253b2ae792d42
fikrialfjri <fikribusiness2021@gmail.com>
2025-10-30 11:01:35 +0700

    Client: Setup typography tailwind utilities

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <main className="app-container">Utangin</main>;
+  return (
+    <main className="app-container">
+      <p className="typo-title-xl">Utangin</p>
+    </main>
+  );
 };
 
 export default App;
