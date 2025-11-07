b1657fc7acc63f9e8c3dffa009cd99b97bb53aa2
fikrialfjri <fikribusiness2021@gmail.com>
2025-11-07 20:13:45 +0700

    Client: Setup vite-tsconfig-paths

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,10 @@
-import path from 'node:path';
-
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
